381231edeb7d53138b3d24a2f38836ecaefdc8ce
Adam Krejci <krejciadam@gmail.com>
2017-11-13 15:32:13 +0100

    new error message, exit codes

--- hdxsmooth/main.py
+++ hdxsmooth/main.py
@@ -10,7 +10,7 @@ def main(argv):
         opts, args = getopt.getopt(argv, "i:o:t:", ["infile=", "outfile=", "times="])
     except getopt.GetoptError:
         print('Error. Invalid arguments')
-        sys.exit(2)
+        sys.exit(1)
     for opt, arg in opts:
         if opt in ("-i", "--infile"):
             infile = arg
@@ -20,17 +20,21 @@ def main(argv):
             times = map(int, arg.split(','))
     if len(outfile) < 1:
         print("Error. Output file not specified.")
-        exit(2)
+        sys.exit(1)
     if len(infile) < 1:
         print("Error. Input file not specified.")
-        exit(2)
+        sys.exit(1)
 
     fragments_map, protein_names = io.load_fragment_file(infile)
     if times is None:
         times = fragments_map.keys()
     positions_map = {}
     for time in times:
-        positions_map[time] = [core.calculate_denaturation(frag_set) for frag_set in fragments_map[time]]
+        try:
+            positions_map[time] = [core.calculate_denaturation(frag_set) for frag_set in fragments_map[time]]
+        except KeyError as e:
+            print("Error. Time not defined in the data: {}".format(e))
+            sys.exit(1)
     io.write_result_table(outfile, positions_map, protein_names)
 
 
