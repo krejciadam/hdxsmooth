ad541fd8fe4088334c9156079045a77cc7d75888
Adam Krejci <krejciadam@gmail.com>
2017-11-20 13:46:04 +0100

    csv loading function

--- hdxsmooth/io.py
+++ hdxsmooth/io.py
@@ -2,8 +2,33 @@ import csv
 from hdxsmooth import core
 
 
+def load_csv_input(path):
+    protein_fragments = {}
+    proline_positions = set()
+    with open(path, newline=None) as file:
+        next(file, None)  # skip header
+        reader = csv.reader(file, delimiter=',', quoting=csv.QUOTE_NONE)
+        for row in reader:
+            protein, start, end, seq, time, deut = row[0], int(row[1]), int(row[2]), row[3], int(row[4]), float(row[7])
+            positions = list(range(start + 1, end + 1)) #start + 1 because N-terminus does not count
+            for i, letter in enumerate(seq):
+                if letter == "P":
+                    proline_positions.add(start + i)
+                    if i > 0:
+                        del(positions[i - 1])
+            fragment = core.Fragment(positions, deut)
+            if time not in protein_fragments.keys():
+                protein_fragments[time] = {protein:[fragment]}
+            elif protein not in protein_fragments[time].keys():
+                protein_fragments[time][protein] = [fragment]
+            else:
+                protein_fragments[time][protein].append(fragment)
+    return protein_fragments
+
+
+
 def load_fragment_file(path, proline_positions = set([]), protein_end=None):
-    with open(path, newline='') as file:
+    with open(path, newline=None) as file:
         reader = csv.reader(file, delimiter=';', quoting=csv.QUOTE_NONE)
         for i, row in enumerate(reader):
             if i == 0: #header
